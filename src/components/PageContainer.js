1ea4d1fcbae01984da06601a234a0d313f0fff1c
LillyIlex <116085080+LillyIlex@users.noreply.github.com>
2023-03-07 14:27:22 +0000

    setting up react app and portfolio containers

---
+++ src/components/PageContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavTabs from './components/NavTabs';
+import Home from './components/pages/Home';
+import Header from './components/pages/Header';
+import Project from './components/pages/Project';
+import Contact from './components/pages/Contact';
+import ProjectGallery from './components/pages/ProjectGallery';
+import projects from './components/pages/projects.json'
+
+function App() {
+  return (
+
+    <Router>
+      <div>
+        <NavTabs />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path='/' element={<Home />} />
+          <Route path='Project' element={<Project />} />
+          {/* <Route path='ProjectGallery' element={<ProjectGallery />} /> */}
+          {/* Define a route that will have descendant routes */}
+          <Route path='contact/*' element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+    
+  );
+}
